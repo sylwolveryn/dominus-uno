8af305bf8f75fd7d7babe9cae093101c5c03b0c1
Marcell Neszt <sylwolveryn@gmail.com>
2020-06-12 14:40:10 +0200

    etst schema

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+exports.optionsSchema = [
+    'discipline',
+    'module',
+    'homework',
+    'uniqueName',
+    'un',   // same as uniqueName
+    'id',   // same as uniqueName
+    ];
+
 exports.test = (options) => {
     console.log(JSON.stringify(options, null, 2));
 };
